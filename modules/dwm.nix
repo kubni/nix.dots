f5da77ce39a409d0cdd278bce02fb8d7c3091a42
kubni <nikola.kuburovic123@gmail.com>
2023-09-30 17:15:44 +0200

    Use my own voidwm repo as dwm src.
    The Makefile had to be patched in order to not mess with /usr
    directories.
    TODO: dwmblocks, fonts...

--- modules/dwm.nix
+++ modules/dwm.nix
@@ -14,10 +14,19 @@
       enable = true;
     };
 
-
-    # Enable dwm
     windowManager.dwm =  {
       enable = true;
+
+      package = pkgs.dwm.overrideAttrs(oldAttrs: {
+        src = pkgs.fetchgit {
+          url = "https://github.com/kubni/voidwm";
+          hash = "sha256-a6u30/6fy7xsmib2ECr5nQFv2P8VmpbyysFbv+G/IlY=";
+        };
+
+        prePatch = oldAttrs.prePatch + ''
+          sed -i "s@/usr/share@$out/share@" Makefile
+        '';
+      });
     };
   };
 
