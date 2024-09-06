4b6991695a59b806477a469b94f96a240a3f5df0
kubni <nikola.kuburovic123@gmail.com>
2024-09-06 13:26:31 +0200

    Enable wireguard for protonvpn

--- modules/configuration.nix
+++ modules/configuration.nix
@@ -8,6 +8,7 @@
   imports =
     [ 
       ./hardware-configuration.nix
+      ./wireguard.nix
       ./emacs
       ./go
     ];
---
+++ modules/wireguard.nix
@@ -0,0 +1,25 @@
+{ pkgs, ...}: 
+
+{
+  networking.wg-quick.interfaces = let
+    server_ip = "93.190.141.58";
+  in {
+    wg0 = {
+      # IP address of this machine in the *tunnel network*
+      address = [ "10.2.0.2/32" ];
+      dns = [ "10.2.0.1" ];
+      # To match firewall allowedUDPPorts (without this wg
+      # uses random port numbers).
+      listenPort = 51820;
+
+      # Path to the private key file.
+      privateKeyFile = "/etc/proton-vpn.key";
+
+      peers = [{
+	publicKey = "SPXmLWGJrr+NAcIALh7VadGZMqw0GZoceGO4VP2MEQo=";
+        allowedIPs = [ "0.0.0.0/0" ];
+        endpoint = "${server_ip}:51820";
+      }];
+    };
+  };
+}
