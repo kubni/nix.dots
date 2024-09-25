c4668a2163315275347a9bb255bb67ac87d0a29a
kubni <nikola.kuburovic123@gmail.com>
2024-09-25 21:49:47 +0200

    WIP: Revert hyprland, separate confs

--- flake.nix
+++ flake.nix
@@ -16,10 +16,10 @@
 	   url = "git+https://github.com/hyprwm/Hyprland?ref=refs/tags/v0.43.0&submodules=1";
    };
 
-   hyprsplit = {
-     url = "github:shezdy/hyprsplit?ref=refs/tags/v0.43.0";
-     inputs.hyprland.follows = "hyprland";
-   };
+   # hyprsplit = {
+   #   url = "github:shezdy/hyprsplit?ref=refs/tags/v0.43.0";
+   #   inputs.hyprland.follows = "hyprland";
+   # };
 
    nvf = {
      url = "github:notashelf/nvf";  
@@ -27,41 +27,55 @@
    };
   };
 
-  outputs = { self, nixpkgs, nixpkgs-unstable, home-manager, hyprland, hyprsplit, nvf}:
+  # outputs = { self, nixpkgs, nixpkgs-unstable, home-manager, hyprland, nvf}: # TODO: hyprsplit
+  outputs = inputs:
     let
       system = "x86_64-linux";
+      
       pkgs = import nixpkgs {
         inherit system;
         config.allowUnfree = true;
       };
+
       pkgs-unstable = import nixpkgs-unstable {
         inherit system;
-	config.allowUnfree = true;
+	      config.allowUnfree = true;
       };
+      
       lib = nixpkgs.lib;
+      
       vars = {
         location = "$HOME/nix.dots";
-	terminal = "kitty";
-	editor = "nvim";
+	      terminal = "kitty";
+	      editor = "nvim";
       };
+
+      pcSetup = import ./hosts/pc;
+      legionSetup = import ./hosts/legion;
+
     in {
       nixosConfigurations = {
-        nikola = lib.nixosSystem {
-          inherit system;
-          specialArgs = { inherit pkgs-unstable vars hyprland; };  
-          modules = [
-            ./modules/configuration.nix
-            nvf.nixosModules.default
-            home-manager.nixosModules.home-manager {
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-	            home-manager.extraSpecialArgs = { inherit pkgs-unstable hyprland hyprsplit; };
-              home-manager.users.nikola = {
-                imports = [ ./home-manager ]; 
-              };
-            }
-          ];
-        };
+        # nikola = lib.nixosSystem {
+        #   inherit system;
+        #   specialArgs = { inherit pkgs-unstable vars hyprland; };
+        #   modules = [
+        #     ./modules/configuration.nix
+        #     nvf.nixosModules.default
+        #     home-manager.nixosModules.home-manager {
+        #       home-manager.useGlobalPkgs = true;
+        #       home-manager.useUserPackages = true;
+	      #       home-manager.extraSpecialArgs = { inherit pkgs-unstable hyprland;}; # TODO: Hyprsplit
+        #       home-manager.users.nikola = {
+        #         imports = [ ./home-manager ];
+        #       };
+        #     }
+        #   ];
+        # };
+
+        pc = pcSetup inputs;
+
+
+
       };
     };
 }
