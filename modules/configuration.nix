6f4eb837d1310a718842db68e88221b716a4dbe3
kubni <nikola.kuburovic123@gmail.com>
2024-10-18 09:50:19 +0200

    Update cache, flake

--- modules/configuration.nix
+++ modules/configuration.nix
@@ -21,11 +21,11 @@
     experimental-features = ["nix-command" "flakes"];
     substituters = [
       "https://hyprland.cachix.org"
-      "https://cuda-maintainers.cachix.org"
+      "https://nix-community.cachix.org"
     ];
     trusted-public-keys = [
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-      "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
   };
     optimise.automatic = true;
