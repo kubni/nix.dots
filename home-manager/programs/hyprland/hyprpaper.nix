5ec5555556b7e39fea6bc9116534ff029703bf1e
kubni <nikola.kuburovic123@gmail.com>
2025-02-14 21:58:50 +0100

    Update flake, change wallpaper

--- home-manager/programs/hyprland/hyprpaper.nix
+++ home-manager/programs/hyprland/hyprpaper.nix
@@ -6,10 +6,11 @@
       splash = false;
       preload = [
       "/home/nikola/Wallpapers/torii.jpg"
+      "/home/nikola/Wallpapers/2b.jpeg"
       ];
       wallpaper = [
-	"DP-3,/home/nikola/Wallpapers/torii.jpg"
-	"HDMI-A-1,/home/nikola/Wallpapers/torii.jpg"
+	"DP-3,/home/nikola/Wallpapers/2b.jpeg"
+	"HDMI-A-1,/home/nikola/Wallpapers/2b.jpeg"
       ];
     };
   };
