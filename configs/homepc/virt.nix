17a814d1e43f4f5988d01117ff4bf03c23a6adc3
kubni <nikola.kuburovic123@gmail.com>
2025-10-19 12:43:49 +0200

    Update system

--- configs/homepc/virt.nix
+++ configs/homepc/virt.nix
@@ -8,14 +8,6 @@
         package = pkgs.qemu_kvm;
         runAsRoot = true;
         swtpm.enable = false;
-        ovmf = {
-          enable = true;
-          # packages = [ pkgs-stable.OVMF.fd ];
-          packages = [ pkgs.OVMF.fd ];
-        };
-        #verbatimConfig = ''
-        #  nvram = [ "${pkgs.OVMF}/FV/OVMF.fd:${pkgs.OVMF}/FV/OVMF_VARS.fd" ]
-        #'';
       };
     };
     spiceUSBRedirection.enable = true;
