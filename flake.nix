163e89a5b652511b62481f6c83a5443d3084746f
kubni <nikola.kuburovic123@gmail.com>
2026-06-05 21:25:42 +0200

    Update system, add claude desktop overlay

--- flake.nix
+++ flake.nix
@@ -1,54 +1,54 @@
 {
   inputs = {
-   nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+      nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-   nixos-hardware = {
-      url = "github:NixOS/nixos-hardware/master";
-   };
+      nixos-hardware = {
+        url = "github:NixOS/nixos-hardware/master";
+      };
 
-   home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+      home-manager = {
+        url = "github:nix-community/home-manager";
+        inputs.nixpkgs.follows = "nixpkgs";
+      };
 
-   nvf = {
-     url = "github:notashelf/nvf";  
-     inputs.nixpkgs.follows = "nixpkgs";
-   };
-   hyprland = {
-      # url = "git+https://github.com/hyprwm/Hyprland?ref=refs/tags/v0.53.3";
-      url = "git+https://github.com/hyprwm/Hyprland";
-   };
+      nvf = {
+        url = "github:notashelf/nvf";  
+        inputs.nixpkgs.follows = "nixpkgs";
+      };
+      hyprland = {
+        url = "git+https://github.com/hyprwm/Hyprland";
+      };
 
-   hyprsplit = {
-     # url = "github:shezdy/hyprsplit?ref=refs/tags/v0.53.3";
-     url = "github:shezdy/hyprsplit";
-     inputs.hyprland.follows = "hyprland";
-   };
+      hyprsplit = {
+        url = "github:shezdy/hyprsplit";
+        inputs.hyprland.follows = "hyprland";
+      };
 
-   disko = {
-     url = "github:nix-community/disko/latest";
-     inputs.nixpkgs.follows = "nixpkgs";
-   };
+      disko = {
+        url = "github:nix-community/disko/latest";
+        inputs.nixpkgs.follows = "nixpkgs";
+      };
 
-   firefox-addons = {
-     url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
-     inputs.nixpkgs.follows = "nixpkgs";
-   };
-   
-   mango = {
-      url = "github:DreamMaoMao/mango";
-      inputs.nixpkgs.follows = "nixpkgs";
-   };
- 
-   stylix = {
-      url = "github:nix-community/stylix";
-      inputs.nixpkgs.follows = "nixpkgs";
-   };
+      firefox-addons = {
+        url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
+        inputs.nixpkgs.follows = "nixpkgs";
+      };
 
+      mango = {
+        url = "github:DreamMaoMao/mango";
+        inputs.nixpkgs.follows = "nixpkgs";
+      };
+
+      stylix = {
+        url = "github:nix-community/stylix";
+        inputs.nixpkgs.follows = "nixpkgs";
+      };
+
+      # claude-desktop.url = "github:aaddrick/claude-desktop-debian";
+      claude-desktop.url = "github:aaddrick/claude-desktop-debian/5dd948e96d853ed37636bc0e2368fc2665cd1104";
   };
 
-  outputs = { self, nixpkgs, home-manager, nvf, hyprland, hyprsplit, disko, firefox-addons, mango, stylix, nixos-hardware}:
+  outputs = { self, nixpkgs, home-manager, nvf, hyprland, hyprsplit, disko, firefox-addons, mango, stylix, nixos-hardware, claude-desktop}:
     let
       lib = nixpkgs.lib;
       
@@ -72,6 +72,12 @@
             ./configs/homepc/configuration.nix
             ./configs/homepc/disko.nix
             overlaysModule
+            {
+              nixpkgs.overlays = [
+                claude-desktop.overlays.default 
+                (import ./overlays/claude-desktop-python.nix)
+              ];
+            }
             nixos-hardware.nixosModules.gigabyte-b650
             disko.nixosModules.disko
             nvf.nixosModules.default
